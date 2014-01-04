8bb7458e33c539aa3775d3a60013ef76b1c70323
Joe Beda <joe.github@bedafamily.com>
2014-01-03 17:01:26 -0800

    Fix up cloud sdk path

--- source/50_cloud_sdk.sh
+++ source/50_cloud_sdk.sh
@@ -1,5 +1,5 @@
 export CLOUD_SDK_DIR="$HOME/src/google-cloud-sdk"
-if [ -d "$CLOUD_SDK_DIR" ]; then
+if [ -d "${CLOUD_SDK_DIR}" ]; then
     # The next line updates PATH for the Google Cloud SDK.
-    export PATH=$CLOUD_SDK_DIR:$PATH
+    export PATH=${CLOUD_SDK_DIR}/bin:${PATH}
 fi
