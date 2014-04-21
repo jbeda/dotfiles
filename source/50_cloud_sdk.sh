c993b0645dd901fa028f24697cbb99a334766383
Joe Beda <joe.github@bedafamily.com>
2014-04-21 15:59:55 -0700

    Pull in gcloud bash completion if avail.

--- source/50_cloud_sdk.sh
+++ source/50_cloud_sdk.sh
@@ -1,5 +1,8 @@
 export CLOUD_SDK_DIR="$HOME/src/google-cloud-sdk"
 if [ -d "${CLOUD_SDK_DIR}" ]; then
-    # The next line updates PATH for the Google Cloud SDK.
-    export PATH=${CLOUD_SDK_DIR}/bin:${PATH}
+  # The next line updates PATH for the Google Cloud SDK.
+  export PATH=${CLOUD_SDK_DIR}/bin:${PATH}
+fi
+if [ -r "${CLOUD_SDK_DIR}/completion.bash.inc" ]; then
+  source "${CLOUD_SDK_DIR}/completion.bash.inc"
 fi
