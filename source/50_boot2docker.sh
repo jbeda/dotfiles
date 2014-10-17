90f3379488ca4a3145cbc8eae29f4f23fe1fc222
Joe Beda <joe.github@bedafamily.com>
2014-10-17 12:41:08 -0700

    boot2docker 1.3

--- source/50_boot2docker.sh
+++ source/50_boot2docker.sh
@@ -1,6 +1,6 @@
 if type boot2docker >& /dev/null; then
   if [[ $(boot2docker status) == "running" ]]; then
-    export DOCKER_HOST=tcp://$(boot2docker ip 2>/dev/null):2375
+    $(boot2docker shellinit 2>/dev/null)
   else
     echo "WARNING: boot2docker not running."
   fi
