c38bc1ade6b2fe4e15615697c4f49cbae62032e3
Joe Beda <joe.github@bedafamily.com>
2015-04-01 10:32:09 -0700

    Reuse existing ssh agent if you can

--- source/50_ssh_agent.sh
+++ source/50_ssh_agent.sh
@@ -18,12 +18,14 @@ function start_agent {
 }
 
 # Source SSH settings, if applicable
-if [ -f "${SSH_ENV}" ]; then
-     . "${SSH_ENV}" > /dev/null
-     #ps ${SSH_AGENT_PID} doesn't work under cywgin
-     ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
+if ! ssh-add -l &> /dev/null; then
+    if [ -f "${SSH_ENV}" ]; then
+         . "${SSH_ENV}" > /dev/null
+         #ps ${SSH_AGENT_PID} doesn't work under cywgin
+         ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
+             start_agent
+         }
+    else
          start_agent
-     }
-else
-     start_agent
+    fi
 fi
