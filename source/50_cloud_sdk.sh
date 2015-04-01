0ae78cb0d486c510e197cdcf300d3f07b064302e
Joe Beda <joe.github@bedafamily.com>
2015-04-01 10:12:54 -0700

    Add gce alias

--- source/50_cloud_sdk.sh
+++ source/50_cloud_sdk.sh
@@ -2,6 +2,8 @@ export CLOUD_SDK_DIR="$HOME/src/google-cloud-sdk"
 if [ -d "${CLOUD_SDK_DIR}" ]; then
   # The next line updates PATH for the Google Cloud SDK.
   export PATH=${CLOUD_SDK_DIR}/bin:${PATH}
+
+  alias gce="gcloud compute"
 fi
 if [ -r "${CLOUD_SDK_DIR}/completion.bash.inc" ]; then
   source "${CLOUD_SDK_DIR}/completion.bash.inc"
