c09ce13205f80c686aea05b92183167e43a0b4c9
Joe Beda <joe.github@bedafamily.com>
2021-11-09 09:53:11 -0800

    Fix up AWS env helper func
    Signed-off-by: Joe Beda <joe.github@bedafamily.com>

--- source/50_aws_env.sh
+++ source/50_aws_env.sh
@@ -1,6 +1,8 @@
 function aws-load-env {
     local profile="${1:-default}"
 
+    export TEST_FOO=FOO
+
     if [[ ${profile} == clear ]]; then
         unset AWS_ACCESS_KEY_ID
         unset AWS_SECRET_ACCESS_KEY
@@ -8,14 +10,10 @@ function aws-load-env {
         unset AWS_SECRET_KEY
         echo "Cleared AWS env variables"
     else
-        local AWS_ACCESS_KEY_ID="$(aws configure get aws_access_key_id --profile ${profile})"
-        local AWS_SECRET_ACCESS_KEY="$(aws configure get aws_secret_access_key --profile ${profile})"
-        local AWS_SESSION_TOKEN="$(aws configure get aws_session_token --profile ${profile})" 
-        local AWS_SECRET_KEY=${AWS_SECRET_ACCESS_KEY}
-        export AWS_ACCESS_KEY_ID
-        export AWS_SECRET_ACCESS_KEY
-        export AWS_SESSION_TOKEN
-        export AWS_SECRET_KEY
+        export AWS_ACCESS_KEY_ID="$(aws configure get aws_access_key_id --profile ${profile})"
+        export AWS_SECRET_ACCESS_KEY="$(aws configure get aws_secret_access_key --profile ${profile})"
+        export AWS_SESSION_TOKEN="$(aws configure get aws_session_token --profile ${profile})" 
+        export AWS_SECRET_KEY=${AWS_SECRET_ACCESS_KEY}
         echo "Set AWS env variables for profile '${profile}'"
     fi
 }
